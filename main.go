8b1053035f2ca3efefe2e960b750958a24191d55
Egor <189146166+10kkyvl@users.noreply.github.com>
2026-08-20 14:53:11 +0300

    chore: scaffold wails v3 app with svelte frontend

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := application.New(application.Options{
+		Name:        "Aurora",
+		Description: "Aurora game launcher",
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(assets),
+		},
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+	})
+
+	app.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title:            "Aurora",
+		Width:            1440,
+		Height:           900,
+		MinWidth:         1000,
+		MinHeight:        680,
+		Frameless:        true,
+		BackgroundColour: application.NewRGB(11, 16, 22),
+		Mac: application.MacWindow{
+			InvisibleTitleBarHeight: 50,
+			Backdrop:                application.MacBackdropTranslucent,
+			TitleBar:                application.MacTitleBarHiddenInset,
+		},
+		URL: "/",
+	})
+
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
